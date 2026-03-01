156087f3e7bbfebb35de99bdd82a919e216de07d
MD SHOUROV HASAN <shourov5275@gmail.com>
2026-03-01 10:08:05 +0600

    Finished writing notes for OOP

--- Object-Oriented Programming.py
+++ Object-Oriented Programming.py
@@ -8,10 +8,10 @@
 #for our object.
 
 class Character:
-    def __init__(self, Name, XPosition, YPosition):
-        self.__Name = Name #String
-        self.__XPosition = XPosition #Integer
-        self.__YPosition = YPosition #Integer
+    def __init__(self, PName, PXPosition, PYPosition):
+        self.__Name = PName #String
+        self.__XPosition = PXPosition #Integer
+        self.__YPosition = PYPosition #Integer
 
     def GetXPosition(self):
         return self.__XPosition
@@ -44,16 +44,44 @@ class Character:
             self.SetXPosition(self.GetXPosition() + 10)
 
 class BikeCharacter(Character):
-# To be continued
+    def __init__(self, PName, PXPosition, PYPosition):
+        super().__init__(PName, PXPosition, PYPosition)
+        #we are inheriting the attributes from parent class
 
 
+    def Move(self, direction):
+        #we are modifying the parent's method, which is an example of polymorphism
+        #by overriding the Move() method
 
-
-
+        if direction == "up":
+            self.SetYPosition(self.GetYPosition() + 20)
+        elif direction == "down":
+            self.SetYPosition(self.GetYPosition() - 20)
+        elif direction == "left":
+            self.SetXPosition(self.GetXPosition() - 20)
+        elif direction == "right":
+            self.SetXPosition(self.GetXPosition() + 20)
 
 #main program
 
 Jack = Character("Jack", 50, 50)
+Karla = BikeCharacter("Karla",100,50)
+
+User_character = input("Enter a character name you would like to move: ")
+
+while User_character != "jack" and User_character != "karla":
+    User_character = input("Invalid input. Try again: ")
+
+User_direction = input("Enter a direction (up/down/left/right): ")
+
+while User_direction != "up" and User_direction != "down" and User_direction != "left" and User_direction != "right":
+    User_direction = input("Invalid input. Try again: ")
+
+if User_character == "jack":
+    Jack.Move(User_direction)
+else:
+    Karla.Move(User_direction)
+
 
 
 
